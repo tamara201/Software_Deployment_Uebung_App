55041d0d528dadf321d8acb0d742122f20386e25
dawidstyczynski <66594681+dawidstyczynski@users.noreply.github.com>
2022-12-14 18:18:04 +0100

    changed front page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello Stefan!')
+  res.send('Hello World!')
 })
 
 //Launch listening server on port 8080
