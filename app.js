c091a38da75f1b5f86faad979a7c9068fc0cd340
dawidstyczynski <66594681+dawidstyczynski@users.noreply.github.com>
2022-12-14 18:40:58 +0100

    changed front page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express()
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
   //content = '<html><h1>Hello World!</h1></html>'
-  content = '<html><h1>Hello World!</h1><br><img src=”/images/unknown.jpg”><br><button id="myButton" class="float-left submit-button" >Die Mächtigkeit!</button><script type="text/javascript">document.getElementById("myButton").onclick = function () { location.href = "https://www.metalevel.at/prolog";};</script></html>'
+  content = '<html><h1>Hello World!</h1><br><img src="/images/unknown.jpg"><br><button id="myButton" class="float-left submit-button" >Die Mächtigkeit!</button><script type="text/javascript">document.getElementById("myButton").onclick = function () { location.href = "https://www.metalevel.at/prolog";};</script></html>'
 
 
   res.send(content)
